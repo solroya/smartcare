041d4150a787210044a433756ee575b2390678f6
ChulHee Kim <solroya@gmail.com>
2025-01-03 14:33:15 +0900

    #16 - 기본컨트롤러 작성

---
+++ src/main/java/com/nado/smartcare/notice/controller/NoticeController.java
@@ -0,0 +1,11 @@
+package com.nado.smartcare.notice.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/erp")
+public class NoticeController {
+
+
+}
