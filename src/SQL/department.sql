6960c8c7b9ac63cda8b2524d751cec5c37c1a49d
ChulHee <solroya@gmail.com>
2025-01-08 11:14:25 +0900

    #31 - 테스트용 쿼리문 작성

---
+++ src/SQL/department.sql
@@ -0,0 +1,16 @@
+-- 내과 등록
+INSERT INTO department (department_id, department_name)
+VALUES (1, 'INTERNAL_MEDICINE');
+
+-- 외과 등록
+INSERT INTO department (department_id, department_name)
+VALUES (2, 'SURGERY');
+
+-- 정신과 등록
+INSERT INTO department (department_id, department_name)
+VALUES (3, 'PSYCHIATRY');
+
+-- 인사과 등록
+INSERT INTO department (department_id, department_name)
+VALUES (4, 'HUMAN_RESOURCE');
+
