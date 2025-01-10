24ac629758ec274f69cbe2a1323f1acd14ac36cd
ChulHee <solroya@gmail.com>
2025-01-10 12:57:28 +0900

    #31 - 쿼리 업데이트

---
+++ src/SQL/DiseaseCategory.sql
@@ -0,0 +1,3 @@
+INSERT INTO disease_category (disease_category_no, category_name) VALUES (1, 'INTERNAL_MEDICINE(내과)');
+INSERT INTO disease_category (disease_category_no, category_name) VALUES (2, 'SURGERY(외과)');
+INSERT INTO disease_category (disease_category_no, category_name) VALUES (3, 'PSYCHIATRY(정신과)');
