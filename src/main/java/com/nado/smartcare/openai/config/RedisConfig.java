4a86402784ab424e600d8c8c92dafef65118531e
ChulHee <solroya@gmail.com>
2025-02-03 12:20:21 +0900

    RedisConfig 하드 코딩 수정

--- src/main/java/com/nado/smartcare/openai/config/RedisConfig.java
+++ src/main/java/com/nado/smartcare/openai/config/RedisConfig.java
@@ -6,6 +6,7 @@ import org.springframework.ai.openai.api.OpenAiApi;
 import org.springframework.ai.vectorstore.RedisVectorStore;
 import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import redis.clients.jedis.JedisPooled;
@@ -19,9 +20,15 @@ public class RedisConfig {
 /*    docker run -d --name redis-stack -p 6379:6379 -p 8001:8001 redis/redis-stack:latest*/
 
 
+    @Value("${spring.data.redis.host}")
+    private String redisHost;
+
+    @Value("${spring.data.redis.port}")
+    private int redisPort;
+
     @Bean
     public JedisPooled jedisPooled() {
-        return new JedisPooled("localhost", 6379);
+        return new JedisPooled(redisHost, redisPort);
     }
 
     @Bean(name = "customVectorStore")
