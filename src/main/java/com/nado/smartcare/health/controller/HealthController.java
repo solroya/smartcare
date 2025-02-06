fb06ce40f9185d68c488ff61cbe13788e04cd1c3
monerc <zguglez@gmail.com>
2025-02-06 20:38:07 +0900

    import 수정

--- src/main/java/com/nado/smartcare/health/controller/HealthController.java
+++ src/main/java/com/nado/smartcare/health/controller/HealthController.java
@@ -1,8 +1,6 @@
 package com.nado.smartcare.health.controller;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Controller;
@@ -26,7 +24,7 @@ public class HealthController {
 	private final ICategoryService iCategoryService; 
 	
 	@GetMapping("/health")
-	public String healthForm(@RequestParam(name = "memberNo", required = false) Long memberNo, Model model) {
+	public String healthForm(@RequestParam("memberNo") Long memberNo, Model model) {
 		log.info("healthController에 들어왔나?");
 		List<CategoryDTO> categories = iCategoryService.getCategoriesForMember(memberNo);
 		log.info("회원 {}에게 추천된 카테고리: {}", memberNo, categories);
--- src/main/java/com/nado/smartcare/member/repository/MemberRepository.java
+++ src/main/java/com/nado/smartcare/member/repository/MemberRepository.java
@@ -6,7 +6,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.lang.foreign.Linker.Option;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
